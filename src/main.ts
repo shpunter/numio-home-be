352e493b3e447b9cf25fe933fe11bb87e0eed7a9
Oleksandr <shpunterman@gmail.com>
2025-05-11 19:44:54 +0200

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { Hono } from "jsr:@hono/hono";
+import { cors } from "jsr:@hono/hono/cors";
 
 const app = new Hono();
+
+app.use('/api/*', cors())
+app.use(
+  '/api/*',
+  cors({
+    origin: '*',
+    allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests'],
+    allowMethods: ['POST', 'GET', 'OPTIONS'],
+    exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+    maxAge: 600,
+    credentials: true,
+  })
+)
 
 app.get("/api/hey", (c) => c.text("Hello Deno!"));
 
